7e95d7aebd953eb36e51e31577d73001c48f8cab
EC2 Default User <ec2-user@ip-172-31-27-137.ec2.internal>
2022-04-05 13:50:35 +0000

    いいね機能実装

---
+++ app/controllers/favorites_controller.rb
@@ -0,0 +1,14 @@
+class FavoritesController < ApplicationController
+  def create
+    favorite = Favorite.new(book_id: params[:book_id], user_id: current_user.id)
+    favorite.save
+    redirect_to request.referer
+  end
+
+  def destroy
+    book = Book.find(params[:book_id])
+    favorite = current_user.favorites.find_by(book_id: book.id)
+    favorite.destroy
+    redirect_to request.referer
+  end
+end
